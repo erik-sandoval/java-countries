d8b6827c6b49dda438fce962a14426baed948f41
Erik Sandoval <erik.sandoval@icloud.com>
2020-01-14 21:33:14 -0800

    created Country Class

---
+++ countries/src/main/java/com/example/countries/Country.java
@@ -0,0 +1,60 @@
+package com.example.countries;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Country {
+
+    private static AtomicInteger currId;
+    private long countryId;
+    private String countryName;
+    private int landMassSize;
+    private int population;
+    private int medianAge;
+
+    public Country(String countryName, int landMassSize, int population, int medianAge) {
+        this.countryName = countryName;
+        this.landMassSize = landMassSize;
+        this.population = population;
+        this.medianAge = medianAge;
+    }
+
+    public long getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(long countryId) {
+        this.countryId = countryId;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public int getLandMassSize() {
+        return landMassSize;
+    }
+
+    public void setLandMassSize(int landMassSize) {
+        this.landMassSize = landMassSize;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public int getMedianAge() {
+        return medianAge;
+    }
+
+    public void setMedianAge(int medianAge) {
+        this.medianAge = medianAge;
+    }
+}
